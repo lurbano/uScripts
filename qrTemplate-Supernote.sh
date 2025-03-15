91aa4295390626696320c36028ceb91709be67e0
lurbano <lurbano@earthsciweb.org>
2025-03-15 18:39:06 -0500

    style

--- qrTemplate-Supernote.sh
+++ qrTemplate-Supernote.sh
@@ -2,5 +2,6 @@
 
 # generate qr code for "https://soriki.com/uSciCalc/" in file sciCalc-qr.png
 qrencode -s 6 -o sciCalc-qr.png "https://soriki.com/uSciCalc/"
+
 # place image in NorthEast corner of canvas for supernote Nomad
 composite -gravity NorthEast -geometry +50+100 sciCalc-qr.png -size 1404x1872 canvas:none sciCalc-template.png
